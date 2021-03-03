0eaba5c7306b4780b108cc216a6bd632c18222f0
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-03-02 21:20:34 -0500

    completed the lab and passed all tests

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -1,26 +1,86 @@
 package rocks.zipcodewilmington.tictactoe;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] tempBoard;
     public Board(Character[][] matrix) {
+    this.tempBoard = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        if(this.tempBoard[0][0] == 'X' && this.tempBoard[0][0] == this.tempBoard[0][1] && this.tempBoard[0][1] == this.tempBoard[0][2]) {
+            return true;
+        } else if (this.tempBoard[1][0] == 'X' && this.tempBoard[1][0] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[1][2]) {
+            return true;
+        } else if (this.tempBoard[2][0] == 'X' && this.tempBoard[2][0] == this.tempBoard[2][1] && this.tempBoard[2][1] == this.tempBoard[2][2]) {
+            return true;
+        } else if (this.tempBoard[0][0] == 'X' && this.tempBoard[0][0] == this.tempBoard[1][0] && this.tempBoard[1][0] == this.tempBoard[2][0]) {
+            return true;
+        } else if (this.tempBoard[0][1] == 'X' && this.tempBoard[0][1] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[2][1]) {
+            return true;
+        } else if (this.tempBoard[0][2] == 'X' && this.tempBoard[0][2] == this.tempBoard[1][2] && this.tempBoard[1][2] == this.tempBoard[2][2]) {
+            return true;
+        } else if (this.tempBoard[0][0] == 'X' && this.tempBoard[0][0] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[2][2]) {
+            return true;
+        } else if (this.tempBoard[0][2] == 'X' && this.tempBoard[0][2] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[2][0]) {
+            return true;
+        } else return false;
+
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+
+        if(this.tempBoard[0][0] == 'O' && this.tempBoard[0][0] == this.tempBoard[0][1] && this.tempBoard[0][1] == this.tempBoard[0][2]) {
+            return true;
+        } else if (this.tempBoard[1][0] == 'O' && this.tempBoard[1][0] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[1][2]) {
+            return true;
+        } else if (this.tempBoard[2][0] == 'O' && this.tempBoard[2][0] == this.tempBoard[2][1] && this.tempBoard[2][1] == this.tempBoard[2][2]) {
+            return true;
+        } else if (this.tempBoard[0][0] == 'O' && this.tempBoard[0][0] == this.tempBoard[1][0] && this.tempBoard[1][0] == this.tempBoard[2][0]) {
+            return true;
+        } else if (this.tempBoard[0][1] == 'O' && this.tempBoard[0][1] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[2][1]) {
+            return true;
+        } else if (this.tempBoard[0][2] == 'O' && this.tempBoard[0][2] == this.tempBoard[1][2] && this.tempBoard[1][2] == this.tempBoard[2][2]) {
+            return true;
+        } else if (this.tempBoard[0][0] == 'O' && this.tempBoard[0][0] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[2][2]) {
+            return true;
+        } else if (this.tempBoard[0][2] == 'O' && this.tempBoard[0][2] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[2][0]) {
+            return true;
+        } else return false;
     }
 
     public Boolean isTie() {
-        return null;
+
+        if(this.tempBoard[0][0] == this.tempBoard[0][1] && this.tempBoard[0][1] == this.tempBoard[0][2]) {
+            return false;
+        } else if (this.tempBoard[1][0] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[1][2]) {
+            return false;
+        } else if (this.tempBoard[2][0] == this.tempBoard[2][1] && this.tempBoard[2][1] == this.tempBoard[2][2]) {
+            return false;
+        } else if (this.tempBoard[0][0] == this.tempBoard[1][0] && this.tempBoard[1][0] == this.tempBoard[2][0]) {
+            return false;
+        } else if (this.tempBoard[0][1] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[2][1]) {
+            return false;
+        } else if (this.tempBoard[0][2] == this.tempBoard[1][2] && this.tempBoard[1][2] == this.tempBoard[2][2]) {
+            return false;
+        } else if (this.tempBoard[0][0] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[2][2]) {
+            return false;
+        } else if (this.tempBoard[0][2] == this.tempBoard[1][1] && this.tempBoard[1][1] == this.tempBoard[2][0]) {
+            return false;
+        } else return true;
     }
 
     public String getWinner() {
-        return null;
+        if(isInFavorOfX() == true) {
+            return "X";
+        } else if(isInFavorOfO() == true) {
+            return "O";
+        } else return "";
     }
 
 }
